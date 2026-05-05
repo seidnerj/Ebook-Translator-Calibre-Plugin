496c1b1ad481ec9f14ee7d8a7f5035fd8d545683
seidnerj <4147381+seidnerj@users.noreply.github.com>
2026-05-05 17:03:32 +0300

    chore: bump local version to 2.4.22 for testing identification

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ class EbookTranslator(InterfaceActionBase):
     supported_platforms = ['windows', 'osx', 'linux']
     identifier = 'ebook-translator'
     author = 'bookfere.com'
-    version = (2, 4, 21)
+    version = (2, 4, 22)
     __version__ = 'v' + '.'.join(map(str, version))
     description = _(
         'A Calibre plugin to translate ebook into a specified language '
